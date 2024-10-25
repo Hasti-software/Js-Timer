f86c19a46119a923a51e27b1c5674ecbabaa3c03
Hasti-sofware <hbahrami883@gmail.com>
2024-10-25 12:27:06 +0330

    first commit of timer project

---
+++ timer.js
@@ -0,0 +1,17 @@
+var usermin = +prompt('enter your min : ')
+var usersec = +prompt('enter your secend : ')
+
+var timer = setInterval(function () {
+    if (usersec === -1 ){
+       usermin--
+       usersec = 59
+    }
+
+    if (usermin === 0 && usersec === 0) {
+        clearInterval(timer)
+        alert('time is up')
+    }
+
+    console.log(usermin + ' : ' + usersec)
+    usersec--
+}, 1000)
